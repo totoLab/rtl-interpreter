a04d8a21b8ceef6d48d0e8377aa1d50063df45b8
toto <antoniolabate19@gmail.com>
2022-06-23 17:21:42 +0200

    including ram in debugging

--- lib.py
+++ lib.py
@@ -1,4 +1,4 @@
-def printRegisters(registers):
+def printRegisters(registers, ram):
     maxLen = 0
     for register in registers:
         if len(register) > maxLen:
@@ -8,6 +8,18 @@ def printRegisters(registers):
         spacing = " " * (maxLen - len(register))
         value = registers[register]
         print(f"{register}{spacing} | {value}")
+
+    print("RAM:")
+    if len(ram) == 0:
+        print("Unused.")
+    else:
+        for addr in ram:
+            spacing = " " * (8 - len(addr))
+            value = ram[addr]
+            print(f"{addr}{spacing} | {value}")
+    print("-------")
+
+
 def extractArgs(expr, operator):
     args = expr.split(operator)
     for i in range(len(args)):
